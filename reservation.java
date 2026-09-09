fa0edf4bce6fe47b18163fdf3679187f8328b652
laminezare <lamine.zare@2ie-edu.org>
2026-09-09 10:33:21 +0000

    Refactor reservation class and fix variable names

--- reservation.java
+++ reservation.java
@@ -1,21 +1,23 @@
-public class reservation {
+public class Reservation {
     private int numero_billet;
-    private vols vol;
+    private Vol vol;
     private Passager passagers;
-    private boolean confirm_vol;
-    public reservation(int numero_billet, vols vol, Passager passagers, boolean confirm_vol){
+    private boolean confirm_vol =false;
+    
+
+    public Reservation(int numero_billet, Vol vol, Passager passagers, boolean confirm_vol){
         this.numero_billet=numero_billet;
         this.confirm_vol=confirm_vol;
         this.vol=vol;
         this.passagers=passagers;
     }
     public void confirm_reservation(){
-        confirm_vol=true;
+        this.confirm_vol=true;
     }
     public void afficher_reservation(){
         System.out.println("numero billet : "+numero_billet);
         System.out.println("vol : "+ vol);
         System.out.println("passager :"+passagers);
-        System.out.println("reservation confirmee :"+comfirm_vol);
+        System.out.println("reservation confirmee :"+confirm_vol);
     }
 }
