d9c93502030f69b853c65e31a8e8a71b58471020
fatoumatadiabate2489 <faimadiabae@gmail.com>
2026-09-26 13:47:35 +0000

    Classe Passager actualisée

--- src/Passager.java
+++ src/Passager.java
@@ -1,14 +1,29 @@
-public class Passager {
-    private String nom;
-    private String prenom;
-    private String passeport;
+public abstract class Passager {
+    protected String nom;
+    protected String prenom;
+    protected String passeport;
     public Passager(String nom,String prenom,String passeport) {
         this.nom=nom;
         this.prenom=prenom;
         this.passeport=passeport;
     }
 
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getPasseport() {
+        return passeport;
+    }
+
     public void afficherPassager() {
         System.out.println("Passager:"+ prenom +" "+nom+"(Passeport:"+ passeport + ")");
     }
+
+    // Le passager standard ne bénéficie d'aucune réduction (0%)
+    public abstract double calculerReduction();
 }
